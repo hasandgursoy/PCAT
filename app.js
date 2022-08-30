179675404badc9692a0bbab2a39299f756f08050
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-30 22:39:13 +0300

    MiddleWare

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const fn = function ()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const myLogger1 = (req, res, next) =>
 {
-    console.log("Function Working")
+    console.log("Middleware Log 1");
+    next();
 }
+const myLogger2 = (req, res, next) =>
+{
+    console.log("Middleware Log 2");
+    next();
+}
+
+// MIDDLEWARES
+// Request ve Response arasında bulunan herşey middleware'dir.
+
+app.use(express.static('public'));
+app.use(myLogger1);
+app.use(myLogger2);
+
+app.get('/', (req, res) =>
+{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+});
+
+
+const port = 3000;
+app.listen(port, () =>
+{
+    console.log(`Sunucu ${port} portunda başlatıldı...`)
+});
