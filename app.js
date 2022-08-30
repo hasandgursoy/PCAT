b07c462cb7f523794f605e6351b1085ed7ee5ca9
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-31 00:16:52 +0300

    Template Engine EJS

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
+
+// TEMPLATE ENGINE
+// Express'e diyoruz ki view engine olarak ejs kullanıcaz.
+app.set("view engine","ejs")
 
 const myLogger1 = (req, res, next) =>
 {
@@ -16,15 +21,28 @@ const myLogger2 = (req, res, next) =>
 // MIDDLEWARES
 // Request ve Response arasında bulunan herşey middleware'dir.
 
+// Bu middleware static dosyaları yakalar.
+// Ancak dinamik bir yapıyı yakalayamayız.
+// Bunun için template engine kavramına ihtiyacımız var.
 app.use(express.static('public'));
 app.use(myLogger1);
 app.use(myLogger2);
 
+
+// ROUTES
 app.get('/', (req, res) =>
 {
-    res.sendFile(path.resolve(__dirname,'temp/index.html'));
+    res.render('index');
 });
 
+app.get('/about', (req, res) =>
+{
+    res.render('about');
+});
+app.get('/add', (req, res) =>
+{
+    res.render('add');
+});
 
 const port = 3000;
 app.listen(port, () =>
